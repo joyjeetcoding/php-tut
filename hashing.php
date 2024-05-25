dbece292ecddc00865998cc03852773560584e0f
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-05-25 19:11:08 +0530

    Added new files

---
+++ hashing.php
@@ -0,0 +1,12 @@
+<?php
+    //hashing
+    $password = "pizza123";
+
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    if(password_verify("pizza123", $hash)){
+        echo "You are logged in";
+    }
+    else {
+        echo "Incorrect Password";
+    }
