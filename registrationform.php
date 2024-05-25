c076dfc99a269b5be1ef4a5edfd1d304aee82560
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-05-26 02:51:05 +0530

    php new codes added

---
+++ registrationform.php
@@ -0,0 +1,52 @@
+<?php
+include("database.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>
+            Welecome to FakeBook
+        </h2>
+        Username:<br>
+        <input type="tekxt" name="username"><br>
+        password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="Register">
+    </form>
+</body>
+
+</html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($username)) {
+            echo "Please enter username";
+        } elseif(empty($password)) {
+            echo "Please enter password";
+        }
+
+        else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "insert into users (user, password) values ('$username', '$hash')";
+
+            mysqli_query($conn, $sql);
+            echo "You are now registered";
+        }
+    }
+
+mysqli_close($conn);
